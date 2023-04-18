bd7388f2fd8d477e9bcae33e6260631adfde529e
middle1 <rna050906@gmail.com>
2023-04-18 09:42:32 +0300

    bin 8.0.12

---
+++ README.md
@@ -0,0 +1,3 @@
+# bin
+<h2>Main:</h2>
+<h3>This is a ready binary php 8.0.12 for minecraft server PocketMine-MP.</h3>
---
+++ bin/php7/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/var/www/svetlana/data/pmmp/bin/php7/lib"
+XML2_LIBS="-lxml2 -L/var/www/svetlana/data/pmmp/bin/php7/lib -lz     -lm "
+XML2_INCLUDEDIR="-I/var/www/svetlana/data/pmmp/bin/php7/include/libxml2"
+MODULE_VERSION="xml2-2.9.13"
+
